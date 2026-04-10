3bff43cd48ef1889154170a3e753a33dca51b4c6
Mawaddah <maw12032004@gmail.com>
2026-04-10 23:12:08 +0700

    Add files via upload

---
+++ Per 6/Per 6 Latihan 2.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<?php
+$x = array("one", "two", "three");
+foreach ($x as $value) {
+    echo $value . "<br />";
+}
+
+$b["sayur"] = "wortel";
+$b["daging"] = "ayam";
+$b["utama"] = "nasi";
+$jumlah = sizeof($b);
+print "Jumlah array b = $jumlah <br>";
+?>
+
+</body>
+</html>
