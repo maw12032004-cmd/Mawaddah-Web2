be928d7446e4437ca39088f8c51a4c54c80814f2
Mawaddah <maw12032004@gmail.com>
2026-06-13 00:52:56 +0700

    Add files via upload

---
+++ Per 18/error3.php
@@ -0,0 +1,10 @@
+<?php
+$a = 5;
+
+if ($a > 0)
+    $status = "Positif"
+else
+    $status = "Negatif";
+
+echo $status;
+?>
---
+++ Per 18/perbaikan_error3.php
@@ -0,0 +1,14 @@
+<?php
+$a = 5;
+
+if ($a > 0)
+{
+    $status = "Positif";
+}
+else
+{
+    $status = "Negatif";
+}
+
+echo $status;
+?>
